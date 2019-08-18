4654ea1340701ca8b61b86be14d89c6b495c8d9f
fayedraza <fayedraza@fayeds-mbp.fios-router.home>
2019-08-17 23:55:29 -0400

    Commit

---
+++ predicate/src/predicate/Employee.java
@@ -0,0 +1,15 @@
+package predicate;
+
+public class Employee {
+
+	public boolean getRatings(int x) {
+		// TODO Auto-generated method stub
+		
+		if(x>10) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
