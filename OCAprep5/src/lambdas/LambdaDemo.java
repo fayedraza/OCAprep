c7629df5964ce80611a8cdce47319d746a87442a
fayedraza <fayedraza@fayeds-mbp.fios-router.home>
2019-08-21 14:53:52 -0400

    Commit

--- OCAprep5/src/lambdas/LambdaDemo.java
+++ OCAprep5/src/lambdas/LambdaDemo.java
@@ -1,11 +1,13 @@
 //created by Fayed Raza
 //programs made in order to prepare for the oca exam
 
+//programs made in order to prepare for the oca exam
 
 package lambdas;
 interface A {
 	void show(int i);
 }
+//programs made in order to prepare for the oca exam
 
 public class LambdaDemo {
      public static void main (String[] args) {
